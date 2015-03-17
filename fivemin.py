e9d6b91c47a6a47617b1d7553e6fd90c3d1ae646
DW Feldman <feldman4@mit.edu>
2015-03-16 21:56:38 -0400

    parse form, create nd array of Reactions

--- fivemin.py
+++ fivemin.py
@@ -1,8 +1,10 @@
 import re
 import pandas as pd
+import numpy as np
 
 test_form = 'reaction_setup.csv'
 concentration_pattern = '([0-9]*\.*[0-9]*)(.*)'
+named_series_pattern = '((.*):)*(.*)'
 
 
 class Experiment(object):
@@ -12,9 +14,11 @@ class Experiment(object):
         :return:
         """
         self.components = []
-        self.conditions = {}
+        self.splits = {}
+        self.reactions = {}
         self.setup(form)
-        self.expand()
+        self.reaction_volume = 10
+
 
     def setup(self, form):
         """Extract Components and desired conditions from pandas DataFrame.
@@ -28,17 +32,57 @@ class Experiment(object):
 
         for experiment in experiments:
             # convert concentrations to fold dilutions
-            conditions = []
-            #TODO allow for "parallel" concentration ranges (A: 1, 2, 3 and A: 1, 1, 2)
-            for entry in form.fillna(value='1X')[experiment]:
-                conditions.append([Concentration(c.strip()) for c in entry.split(',')])
-            self.conditions[experiment] = conditions
+            concentrations = {}
+            index, indices, named_index = 0, {}, {}
+            # singletons will be given index 0
+            split = {0: []}
+            # TODO allow for "parallel" concentration ranges (A: 1, 2, 3 and A: 1, 1, 2)
+            for entry, component in zip(form.fillna(value='1X')[experiment], self.components):
+                # generic format A: 1, 2, 3
+                name, series = re.match(named_series_pattern, entry).groups()[1:3]
+                ingredient = (component, [Concentration(c.strip()) for c in series.split(',')])
+                # assign unique index to each step in the reaction, sort later
+                if name is not None:
+                    if name in named_index:
+                        # previously recognized series
+                        split[named_index[name]].append(ingredient)
+                    else:
+                        # new series
+                        index += 1
+                        named_index[name] = index
+                        split[index] = [ingredient]
+                elif len(ingredient[1]) == 1:
+                    # singleton
+                    split[0].append(ingredient)
+                else:
+                    # unnamed series, gets its own split
+                    index += 1
+                    split[index] = [ingredient]
 
-    def expand(self):
-        for conditions in self.conditions:
-            #TODO recursivley identify next component, applying rank
-            # find singletons
-            [conditions.pop(component) for component in condition if len(component) == 1]
+            split = [split[i] for i in range(index+1)]
+            self.splits[experiment] = split
+            self.reactions[experiment] = self.expand(split)
+
+    @staticmethod
+    def expand(self, split):
+        """Represent experiment layout as ndarray of Reactions with order of axes corresponding to
+        split order. Associate each Component with an axis.
+        :return:
+        """
+        shape = [len(ingredients[0][1]) for ingredients in split]
+        reactions = np.ndarray(shape, dtype=Reaction)
+        it = np.nditer(reactions, op_flags=['readwrite'], flags=['f_index', 'multi_index', 'refs_ok'])
+        for r in it:
+            r[...] = Reaction()
+            for i, split_index in enumerate(it.multi_index):
+                ingredients = split[i]
+                for ingredient in ingredients:
+                    reactions.ravel(order='F')[it.index].add_by_concentration(ingredient[0], ingredient[1][split_index])
+
+        return reactions
+
+    def layout(self):
+        pass
 
 
 class Concentration(object):
@@ -57,17 +101,27 @@ class Concentration(object):
 
 
 class Reaction(object):
-    def __init__(self, components=None, volume=1):
-        self.components = [] if components is None else components
+    def __init__(self, volume=1):
+        self.concentrations = {}
         self.volume = float(volume)
+
+    def add_by_concentration(self, component, concentration):
+        """If Component already in Reaction, overwrites pre-existing Concentration.
+        :param component:
+        :param concentration:
+        :return:
+        """
+        self.concentrations[component] = concentration
 
 
 class Component(object):
     def __init__(self, name, rank, stock):
         self.name = name
         self.rank = rank
-        self.stock = stock.value
+        self.stock = stock
 
+    def __repr__(self):
+        return "%s" % self.name
 
 if __name__ == '__main__':
     form = pd.read_table(test_form, sep=',')
