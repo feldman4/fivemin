6eb8c0632bce473279d949b2746450b764f01a46
DW Feldman <feldman4@mit.edu>
2015-03-20 22:07:57 -0400

    added Expressions

--- fivemin.py
+++ fivemin.py
@@ -1,6 +1,7 @@
-import csv
+import csv, string
 import re
 import pandas as pd
+from collections import defaultdict
 import numpy as np
 import sympy as sp
 
@@ -10,6 +11,13 @@ concentration_pattern = '([0-9]*\.*[0-9]*)(.*)'
 named_series_pattern = '((.*):)*(.*)'
 reaction_volume = 10
 pipette_loss = 1.1
+lc_alphabet = (chr(i).lower() for i in range(65, 65 + 26))
+uc_alphabet = (chr(i) for i in range(65, 65 + 26))
+
+
+def symbol_array(sym, size, assumptions=None):
+    return [sp.symbols(sym + '_' + str(i), **assumptions)
+            for i in range(size)]
 
 
 class Experiment(object):
@@ -19,75 +27,62 @@ class Experiment(object):
         :return:
         """
         self.info = {}
-        self.split_volume = {}
-        self.total_volume = {}
         self.components = []
-        self.splits = {}
         self.reactions = {}
-        self.volumes = {}
         self.reaction_volume = reaction_volume
         self.pipette_loss = pipette_loss
+        self.syms = {}
+        self.layout = lambda: []
+        self.experiments = {}
+        self.expressions = {}
 
         self.setup(form)
-
 
     def setup(self, form):
         """Extract Components and desired conditions from pandas DataFrame.
         :param form:
         :return:
         """
+        # unpack form
         for rank, name, stock in zip(form.fillna(value=0)['rank'], form['reagent'], form['stock']):
-            self.components.append(Component(name=name, rank=rank, stock=Concentration(stock)))
-
+            self.components.append(Component(name=name, rank=rank, concentration=Concentration(stock)))
+            self.syms[self.components[-1]] = uc_alphabet.next()
+        # get experiment columns
         experiments = filter(lambda s: 'experiment' in s, form.columns)
-
+        # determine sub-mixes and components
+        self.experiments = {exp: defaultdict(list) for exp in experiments}
+        experiments = ['experiment 2']
         for experiment in experiments:
             # TODO convert concentrations to fold dilutions
-            index, indices, named_index = 0, {}, {}
-            # singletons will be given index 0
-            split = {0: []}
+            lc_syms, uc_syms, uc_fractions = [], [], {}
             for entry, component in zip(form.fillna(value='1X')[experiment], self.components):
                 # generic format A: 1, 2, 3
                 name, series = re.match(named_series_pattern, entry).groups()[1:3]
-                ingredient = [component, [Concentration(c.strip()) for c in series.split(',')]]
-                # assign unique index to each step in the reaction, sort later
-                if name is not None:
-                    if name in named_index:
-                        # previously recognized series
-                        split[named_index[name]].append(ingredient)
-                    else:
-                        # new series
-                        index += 1
-                        named_index[name] = index
-                        split[index] = [ingredient]
-                elif len(ingredient[1]) == 1:
-                    # singleton
-                    split[0].append(ingredient)
-                else:
-                    # unnamed series, gets its own split
-                    index += 1
-                    split[index] = [ingredient]
+                series = [Concentration(c.strip(), stock=component) for c in series.split(',')]
 
-            split = self.sort_splits([split[i] for i in range(index + 1)])
+                if name is None:
+                    name = 0 if len(series) == 1 else hash(component)
 
-            # calculate volumes
-            #TODO include H2O volumes for 1st split (sum largest volume in each ingredient at each split?)
-            split_sizes = [len(s[0][1]) for s in split]
-            total_volume = np.array([self.pipette_loss ** i *
-                                     self.reaction_volume * np.prod(split_sizes)
-                                     for i in reversed(range(len(split)))])
-            split_volume = np.array(total_volume) / np.cumprod(split_sizes)
-            total_volume = total_volume
+                self.experiments[experiment][name].append({'concentration': series,
+                                                           'component': component})
+            # names no longer important
+            it = self.experiments[experiment].values()
 
-            for ingredients, vol in zip(split, split_volume):
-                for ing in ingredients:
-                    stock_c = ing[0].stock.value
-                    ing.append(np.around([vol * (c.value / stock_c) for c in ing[1]], decimals=2))
-            self.info[experiment] = {'split_volume': split_volume,
-                                     'total_volume': total_volume,
-                                     'split_sizes': split_sizes}
-            self.splits[experiment] = split
-            self.reactions[experiment] = self.expand(split)
+            for submix in it:
+                # lowercase symbols indicate sub-mix in particular experiment
+                lc_syms.append(sp.symarray(lc_alphabet.next(), len(submix)))
+                # uppercase symbols indicate components, subscripts
+                uc_syms.append([symbol_array(self.syms[s['component']], len(s['concentration']), {'positive': True})
+                                for s in submix])
+                # fraction of the final reaction associated with each sub-mix component
+                uc_fractions.update({
+                    n[i]: c.fraction()
+                    for s, n in zip(submix, uc_syms[-1]) for i, c in enumerate(s['concentration'])
+                })
+            self.expressions[experiment] = Expression(lc_syms, uc_syms, uc_fractions)
+
+
+            # split = self.sort_splits([split[i] for i in range(index + 1)])
 
     def sort_splits(self, split):
         # permute, sorting by lowest rank of split then size
@@ -98,32 +93,61 @@ class Experiment(object):
         return sorted_split
 
     def expand(self, split):
-        """Represent experiment layout as ndarray of Reactions with order of axes corresponding to
-        split order. Associate each Component with an axis.
+        """Represent experiment as symbolic expression. When fully expanded, each term represents a final reaction.
+        Intermediate factorizations represent sub-mixes.
         :return:
         """
-        shape = [len(ingredients[0][1]) for ingredients in split]
-        reactions = np.ndarray(shape, dtype=Reaction)
-        it = np.nditer(reactions, op_flags=['readwrite'], flags=['f_index', 'multi_index', 'refs_ok'])
-        for r in it:
-            r[...] = Reaction()
-            for i, split_index in enumerate(it.multi_index):
-                ingredients = split[i]
-                for ingredient in ingredients:
-                    reactions.ravel(order='F')[it.index].add_by_concentration(ingredient[0], ingredient[1][split_index])
-        return reactions
 
-    def layout(self):
-        layout = []
-        for experiment, reactions in self.reactions.items():
-            layout.append([experiment])
-            block_shape = reactions.shape[-2:]
-            if len(block_shape) == 1:
-                block_shape = (1, block_shape[0])
-            new_shape = (reactions.size / block_shape[1], block_shape[1])
-            for line in reactions.reshape(new_shape).tolist():
-                layout.append(line)
-        return layout
+
+class Expression(object):
+    def __init__(self, lc_syms, uc_syms, uc_fractions):
+        self.lc_syms = lc_syms
+        self.uc_syms = uc_syms
+        self.uc_fractions = uc_fractions
+        self.expression = None
+        self.expression_eval = None
+        self.components = {}
+        self.h_values = {}
+
+        self.form_expression()
+        self.pick_h2o()
+
+    def form_expression(self):
+        # form the dictionary relating lowercase symbols to uppercase and water
+        for l_terms, U_terms in zip(self.lc_syms, self.uc_syms):
+            for i, (l, U) in enumerate(reversed(zip(l_terms, zip(*U_terms)))):
+                self.components[l] = sp.exp(sum([V for V in U] +
+                                                [sp.symbols('h_' + str(l), positive=True)]))
+
+        self.expression = sp.prod([sum(s) for s in self.lc_syms])
+        self.expression_eval = sp.expand(self.expression)
+        for var, value in self.components.items():
+            self.expression_eval = self.expression_eval.subs(var, value)
+
+    def pick_h2o(self):
+        """Determine water added in each submix by solving constraints on added water.
+        :return:
+        """
+        h_constraints = []
+        for term in self.expression_eval.subs(self.uc_fractions).args:
+            h_constraints.append(sp.Eq(sp.log(term).simplify(), 1))
+
+        # set free water values to zero
+        self.h_values = {}
+        solved_constraints = sp.solve(h_constraints)
+        if type(solved_constraints) is list:
+            solved_constraints = solved_constraints[0]
+        for lhs, expr in solved_constraints.items():
+            self.h_values.update({h: 0 for h in expr.free_symbols})
+            self.h_values[lhs] = expr.subs(self.h_values)
+
+    def get_submix(self, submix):
+        """Retrieve submix corresponding to tuple. Depends on ordering of lc_syms.
+        :param submix:
+        :return:
+        """
+        split = sp.prod(self.lc_syms[i][j] for i, j in enumerate(submix))
+        return sp.log(split.subs(self.components)).expand()
 
 
 class Instruction(object):
@@ -133,6 +157,7 @@ class Instruction(object):
         self.split_size = 0.
         self.split_volume = 0.
         self.split_label = 'M1'
+
     def get_text(self):
         text1 = 'Split each mix into %d sub-mixes with %.2g uL each.' % \
                 (self.split_size, self.split_volume)
@@ -141,19 +166,27 @@ class Instruction(object):
         text2 = 'Label the new sub-mixes %s.' % ', '.join(mix_labels)
         text3 = 'Add the following to each sub-mix:\n'
         pd.DataFrame()
+
     def get_html(self):
         pass
 
+
 class Concentration(object):
-    def __init__(self, input_string):
+    def __init__(self, input_string, stock=None):
         self.units = None
         self.value = 0.
         self.parse(input_string)
+        self.stock = stock
 
     def parse(self, input_string):
         self.value, self.units = re.match(concentration_pattern, input_string).groups()[:2]
         self.value = float(self.value)
         self.units = self.units.strip()
+
+    def fraction(self):
+        if self.stock is None:
+            return 1
+        return self.value / self.stock.concentration.value
 
     def __repr__(self):
         return "%.2g %s" % (self.value, self.units)
@@ -180,16 +213,13 @@ class Reaction(object):
 
 
 class Component(object):
-    def __init__(self, name, rank, stock):
+    def __init__(self, name, rank, concentration=None):
         self.name = name
         self.rank = rank
-        self.stock = stock
+        self.concentration = Concentration('1X') if concentration is None else concentration
 
     def __repr__(self):
         return "%s" % self.name
-
-class Product(object):
-    pass
 
 
 def test():
