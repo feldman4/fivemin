0a1bb10dbdf793198dac4de3d6413aaeff45f05c
DW Feldman <feldman4@mit.edu>
2015-03-27 01:10:52 -0400

    created organized layout, sped up via subs => xreplace and expand => expand_mul

--- fivemin.py
+++ fivemin.py
@@ -5,6 +5,7 @@ import numpy as np
 import sympy as sp
 from numpy.linalg import lstsq
 import sys
+from time import gmtime, strftime
 
 test_form = 'reaction_tests.csv'
 output_csv = 'output2.csv'
@@ -12,6 +13,7 @@ concentration_pattern = '([0-9]*\.*[0-9]*)(.*)'
 named_series_pattern = '((.*):)*(.*)'
 test_reaction_volume = 10
 test_pipette_loss = 1.1
+pd.options.display.max_columns = 30
 
 
 def symbol_array(sym, size, assumptions=None):
@@ -21,7 +23,7 @@ def symbol_array(sym, size, assumptions=None):
 
 # noinspection PyTypeChecker
 class Experiment(object):
-    def __init__(self, form, reaction_volume=10, pipette_loss=1.):
+    def __init__(self, form, reaction_volume=10, pipette_loss=1., plate_size=(8, 12)):
         """Container for experiment, including all parameters and generated information.
         :param form:
         :return:
@@ -38,7 +40,11 @@ class Experiment(object):
         self.expression = None
         self.uc_alphabet = (chr(i) for i in range(65, 65 + 26))
 
+        self.layout = None
+        self.plate_size = plate_size
+
         self.setup(form)
+        strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
     def setup(self, form):
         """Extract Components and desired conditions from pandas DataFrame.
@@ -66,6 +72,10 @@ class Experiment(object):
                                       'symbol': self.syms[component]})
         self.series = self.series.values()
         self.expression = Expression(self.series)
+        self.series = [self.series[r] for r in self.expression.sorted_order]
+
+    def layout2(self, mode='organized'):
+        self.layout = Layout(self.expression, self.series, mode=mode)
 
     def layout(self, plate_size=(8, 12)):
 
@@ -150,16 +160,21 @@ class Expression(object):
         self.uc_fractions = {}
         self.lc_alphabet = (chr(i).lower() for i in range(65, 65 + 25))
         self.loss = sp.symbols('z')
+        self.sorted_order = []
 
         self.expression = None
         self.expression_eval = None
         self.components = {}
         self.h_values = {}
-
+        print 'start of expr', strftime("%Y-%m-%d %H:%M:%S", gmtime())
         self.define_symbols(submixes)
+        print 'start of sort', strftime("%Y-%m-%d %H:%M:%S", gmtime())
         self.sort()
+        print 'start of form_expr', strftime("%Y-%m-%d %H:%M:%S", gmtime())
         self.form_expression()
+        print 'start of pick_h2o', strftime("%Y-%m-%d %H:%M:%S", gmtime())
         self.pick_h2o()
+        print 'end of pick_h2o', strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
     def define_symbols(self, submixes):
         for submix in submixes:
@@ -188,16 +203,20 @@ class Expression(object):
             skip_h = True
 
         self.expression = sp.prod([sum(s) for s in self.lc_syms])
-        self.expression_eval = sp.expand(self.expression).subs(self.components)
+        print 'before eval', strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        self.expression_eval = sp.expand_mul(self.expression.xreplace(self.components))
 
     def pick_h2o(self):
         """Determine water added in each submix by solving constraints on added water.
         :return:
         """
         h_constraints = []
-        master_equation = self.expression_eval.subs(self.uc_fractions)
+        print 'before the mas subs', strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        master_equation = self.expression_eval.xreplace(self.uc_fractions)
+        print 'after the mas subs', strftime("%Y-%m-%d %H:%M:%S", gmtime())
         for term in master_equation.args:
-            h_constraints.append(sp.Eq(sp.log(term).expand().nsimplify(), 1))
+            h_constraints.append(sp.Eq(sp.log(term).expand(), 1))
+        print 'after the expandsimplify', strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
         # set free water values to zero
         self.h_values = {}
@@ -205,6 +224,7 @@ class Expression(object):
         h_syms = sp.Matrix(list(master_equation.free_symbols))
         M = sp.Matrix([h.args[0].coeff(1) for h in h_constraints]).jacobian(h_syms)
         x = [float(1 - (h.args[0] - h.args[0].coeff(1))) for h in h_constraints]
+        print 'ready for lstsq', strftime("%Y-%m-%d %H:%M:%S", gmtime())
         self.h_values = {h: value for h, value in zip(h_syms, lstsq(M, x)[0])}
 
         # determine final volume for each split
@@ -258,6 +278,7 @@ class Expression(object):
         self.lc_syms = [s for r, s in sorted(zip(rank, self.lc_syms), key=lambda x: x[0])]
         self.uc_syms = [s for r, s in sorted(zip(rank, self.uc_syms), key=lambda x: x[0])]
         self.split_size = tuple([len(l) for l in self.lc_syms])
+        self.sorted_order = [i for r, i in sorted(zip(rank, range(len(rank))))]
 
 
 class Instruction(object):
@@ -275,6 +296,8 @@ class Instruction(object):
         count = self.table.shape[1]
         text1 = 'Transfer %.3g uL to each of %d submix%s.\n' % \
                 (self.split_volume, count, self.plural(count))
+        if count is 1:
+            text1 = ''
         text2 = 'Add the following to each submix:\n'
         return text1 + text2 + '\n' + str(self.table.fillna('-'))
 
@@ -305,27 +328,7 @@ class Concentration(object):
         return self.value / self.stock.concentration.value
 
     def __repr__(self):
-        return "%.2g %s" % (self.value, self.units)
-
-
-class Reaction(object):
-    def __init__(self, volume=1):
-        self.concentrations = {}
-        self.volume = float(volume)
-
-    def add_by_concentration(self, component, concentration):
-        """If Component already in Reaction, overwrites pre-existing Concentration.
-        :param component:
-        :param concentration:
-        :return:
-        """
-        self.concentrations[component] = concentration
-
-    def __repr__(self):
-        representation = []
-        for component, concentration in self.concentrations.items():
-            representation.append(str(component) + ': ' + str(concentration))
-        return "{%s}" % '; '.join(representation)
+        return "%.2g%s" % (self.value, self.units)
 
 
 class Component(object):
@@ -338,15 +341,108 @@ class Component(object):
         return "%s" % self.name
 
 
+class Layout(object):
+    def __init__(self, expression, series, mode='organized', plate_size=(8, 12)):
+        """Organize reactions into a plate format, based around blocks of size (m x n), where m, n
+        are the last two non-singleton splits. In organized mode, group blocks into rows and columns by preceding
+        splits. In compact mode, pack together blocks efficiently without regard for rows and columns.
+        :param Expression expression:
+        :param components:
+        :param str mode:
+        :return:
+        """
+        self.mode = mode
+        self.expression = expression
+        self.series = series
+        self.table = None
+        self.block_size = []
+        self.split_size = expression.split_size
+        self.plate_size = plate_size
+        if mode is 'organized':
+            self.layout_organized()
+        elif mode is 'compact':
+            self.layout_compact()
+
+    def layout_organized(self):
+        lens = lambda x: len(x[0]['concentration'])
+        # define basic block from last two non-singleton splits
+        non_singletons = [s for s in reversed(self.series) if lens(s) > 1]
+        self.block_size = ([lens(s) for s in non_singletons] + [1, 1])[:2]
+        # get list of preceding non-singleton dimensions
+        preceding = non_singletons[2:]
+        # order in rows/columns to minimize plate usage
+        best = ([], np.prod(self.block_size))
+        for i in range(2 ** len(preceding)):
+            order = [int(s) for s in "{0:b}".format(i)]
+            block_size = list(self.block_size)
+            best_order = []
+            for j, (split, row_col) in enumerate(zip(preceding, order)):
+                best_order.append(row_col)
+                block_size[row_col] *= lens(split)
+                overflow = any([b > p for b, p in zip(block_size, self.plate_size)])
+                if overflow or j == len(preceding) - 1:
+                    if overflow:
+                        block_size[row_col] /= lens(split)
+                        best_order.pop()
+                    if np.prod(block_size) > best[1]:
+                        best = (best_order, np.prod(block_size))
+                        continue
+
+        names = lambda x: ['|'.join([str(z['component']) for z in y]) for y in x]
+        conc = lambda x: [['|'.join([str(z['concentration'][i]) for z in y])
+                           for i in range(len(y[0]['concentration']))] for y in x]
+        row_it = [non_singletons[0]] + [p for p, b in zip(preceding, best[0]) if not b]
+        col_it = [non_singletons[1]] + [p for p, b in zip(preceding, best[0]) if b]
+
+        row_index = pd.MultiIndex.from_product(conc(row_it), names=names(row_it))
+        col_index = pd.MultiIndex.from_product(conc(col_it), names=names(col_it))
+
+        base_df = pd.DataFrame(0, index=row_index, columns=col_index)
+        # figure out number of plates and DataFrame on each plate
+        num_plates = np.prod(self.split_size) / best[1]
+        plate_splits = preceding[len(best[0]):]
+        # construct on-plate DataFrame from two MultiIndex
+        plate_dfs = []
+        plate_names = names(plate_splits)
+        for plate in range(num_plates):
+            subsplit = [lens(p) for p in plate_splits]
+            plate_vals = [plate % np.prod(subsplit[:i+1]) for i in range(len(plate_splits))]
+            this_conc = ['|'.join([str(z['concentration'][v]) for z in y])
+                         for y, v in zip(plate_splits, plate_vals)]
+            this_col_index = pd.MultiIndex.from_product(this_conc + conc(col_it),
+                                                        names=plate_names + names(col_it))
+            plate_dfs.append(pd.DataFrame(0, index=row_index, columns=this_col_index))
+
+        self.plate_dfs = plate_dfs
+
+        # # format using MultiIndex
+        # # generate compact, detailed DataFrames
+        # iterables = [['/'.join([str(k['concentration'][i]) for k in s])
+        #               for i in range(len(s[0]['concentration']))] for s in self.series]
+        # names = ['/'.join([str(k['component']) for k in s])
+        #          for s in self.series]
+        # iterables = [it for s, it in zip(self.series, iterables) if len(s[0]['concentration']) > 1]
+        # names = [n for s, n in zip(self.series, names) if len(s[0]['concentration']) > 1]
+        # compact = pd.MultiIndex.from_product(iterables, names=names)
+
+
+    def layout_compact(self):
+        pass
+
+
 def test(filename=test_form):
     form = pd.read_table(filename, sep=',')
+    print strftime("%Y-%m-%d %H:%M:%S", gmtime())
     exp = Experiment(form, reaction_volume=test_reaction_volume,
                      pipette_loss=test_pipette_loss)
 
     exp.write_instructions()
+    exp.layout2()
+    layout = exp.layout.plate_dfs
     with open(filename[:-4] + '_output.txt', 'w') as fh:
         fh.writelines([a + '\n' for a in exp.print_instructions()])
-    exp.layout()
+        fh.writelines([df.to_string() + '\n\n' for df in layout])
+
     return exp
 
 
